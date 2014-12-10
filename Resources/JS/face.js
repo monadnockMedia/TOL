58f8f0cf0c1bcb0bc123081e1048be601b68e71f
W. Ryan Nestor <nestor@fuzzy-math.com>
2014-12-10 15:20:45 -0500

    updates to facetracker

--- Resources/JS/face.js
+++ Resources/JS/face.js
@@ -1,179 +1,203 @@
  var replacer = function(_targets){
+	var self = this;
+	this.imgDir = "./img/"
 	this.targets = _targets;
+	this.targets.forEach(function(t,i){
+		t.paint = new Image();
+		t.paint.src = self.imgDir+t.paint_uri;
+	})
 
-	
-	
 }
 
 replacer.prototype.replace = function(_req){
+	
 	console.log("REPLACE", _req);
 	var debug = false;
-	var dfd = new $.Deferred();
+	var dfd = new $.Deferred();  //main deferred
 	var blendMode = "overlay";
 	var sources = _req.sources;
-	var source;
 	var target = this.targets[_req.tgImageID];
-	var targetFace;
+	var ctx, cnv;
 
-//	var dfd_src = $.Deferred();
-//	var dfd_tg = $.Deferred();
+	var dfd_wm = $.Deferred();
+	var dfd_tg = $.Deferred();
 
-//	$.when(dfd_src,dfd_tg).done(clone_loop);
 	
 	var iTg		= new	Image();
-	var iSrc;
+	var iWm		= new	Image();
 	
-	$(iTg).load(clone_loop)
+	var replacer = this;
+
+	$.when.apply(this,[dfd_tg, dfd_wm])  //when both target image and watermark are loaded
+		.done(function(tg,wm){
+
+			clone_loop.call(replacer).done(function(d){
+				dfd.resolve(d);
+			})
+		});
+
 	
+	$(iTg).load(dfd_tg.resolve);
+	$(iWm).load(dfd_wm.resolve);
+	
+
 	
 	var sources = _req.sources;
 	
 	var cnv,ctx;
-	iTg.src = "./IMAGES/"+target.img_uri;
+	iTg.src = this.imgDir+target.img_uri;
+	iWm.src = this.imgDir+"watermarksm.png";
+
 	
 	
 	function clone_loop(){
+
+		var loop_dfd = new $.Deferred();
+		var self = this;
 		cnv = document.createElement('canvas');
 		ctx = cnv.getContext('2d');
-		cnv.width = iTg.width;		cnv.height = iTg.height;
+		cnv.width = iTg.width;		
+		cnv.height = iTg.height;
 		ctx.drawImage(iTg,0,0);
-		sources.forEach(function(s){
-			iSrc	= new	Image();
-			$(iSrc).load(function(d){clone_one});
-			source = s;
-			iSrc.src = source.face;
-	
-			targetFace = target.faces[source.faceID];
-	
-			clone_one();
+		ctx.drawImage(iWm,0,iTg.height-iWm.height);
+		
+		var dfds = new Array();
+
+		
+		sources.forEach(function(s,i){
+			var tmpI = new Image();
+			s.image = tmpI;
+			dfds.push(new $.Deferred());
+			$(tmpI).load(function(d){
+			
+				dfds[i].resolve();
+			});
+			
+			tmpI.src=s.face;
 		})
 		
 		
-		
-		dfd.resolve(cnv);
-	}
-	
-	function clone_one(){
-		
-		
-		var tW = targetFace.width;
-		var tH = targetFace.height;
-		var tX = targetFace.x;
-		var tY = targetFace.y;
-		var tC = {x:tX+tW/2, y:tY+tH/2};
-		
-		
-		
-		var rect = source.rect;
-		var siW = iSrc.width;
-		var siH = iSrc.height;
-		var sW = rect.width;
-		var sH = rect.height;
-		var sX = rect.x;
-		var sY = rect.y;
-		var sC = {x:sX + (sW/2), y:sY + (sH/2)};
-		
-		var scale = tH/sH;
 
-		var sDW = sW*scale;
-		var sDH = sW*scale;
-		var sDC = {x:rect.x*scale + ((rect.width*scale)/2), y:rect.y*scale + ((rect.height*scale)/2)}
-		
-	//	var siDX = tC.x - (sDC.x/2+sX*scale);
-	//	var siDY = tC.y - (sDC.y/2+sY*scale);
-		
-		var siDX = tX - rect.x*scale;
-		var siDY = tY - rect.y*scale;
+		$.when.apply(self, dfds).done(function(){
+			console.log("loading complete",this);
 			
-	//	var sDX = tC.x - (sDC.x/2);
-	//	var sDY = tC.y - (sDC.y/2);
-		
-		var sDX = tX ;
-		var sDY = tY;
-		
-		
-		if(debug){
-			ctx.strokeStyle="red";
-			ctx.rect(tX,tY, tW, tH);
-			ctx.stroke();
-		}
-		ctx.globalCompositeOperation = blendMode;
-		ctx.drawImage(iSrc,siDX,siDY,siW*scale,siH*scale);
-		
-		if(debug){
-	
+			sources.forEach(function(s,i){
+				clone_one(s)
+			});
+			var res = {
+				
+				dataURL:cnv.toDataURL()
+			}
 			
-			ctx.strokeStyle="purple";
-			ctx.rect(sDX,sDY,sDW,sDH);
-			ctx.stroke();
-		}
 		
+			$.post("http://localhost:2999/saveimg",res).done(function(d){
+				loop_dfd.resolve(d);
+			});
+			
 		
-		dfd.resolve(cnv.toDataURL());
+		});
+		return loop_dfd.promise();
 	}
-	function clone(_src, _tg){
 	
-		console.log("CLONE");
-		var cnv = document.createElement('canvas');
-		var ctx = cnv.getContext('2d');
+	function clone_one(s){
+	
+	//	var clone_dfd = new $.Deferred();
+		this.foo = "bar";
+		var iSrc	= new	Image();
+		var targetFace = target.faces[s.faceID];
+		var iPaint = target.paint;
+		iSrc = s.image;
+		clone();
 		
-		var tW = targetFace.width;
-		var tH = targetFace.height;
-		var tX = targetFace.x;
-		var tY = targetFace.y;
-		var tC = {x:tX+tW/2, y:tY+tH/2};
+		function clone(){
+	
+			var tW = targetFace.width;
+			var tH = targetFace.height;
+			var tX = targetFace.x;
+			var tY = targetFace.y;
+			var tC = {x:tX+tW/2, y:tY+tH/2};
 		
-		cnv.width = iTg.width;		cnv.height = iTg.height;
 		
-		var rect = source.rect;
-		var siW = iSrc.width;
-		var siH = iSrc.height;
-		var sW = rect.width;
-		var sH = rect.height;
-		var sX = rect.x;
-		var sY = rect.y;
-		var sC = {x:sX + (sW/2), y:sY + (sH/2)};
 		
-		var scale = tH/sH;
+			var rect = s.rect;
+			var siW = iSrc.width;
+			var siH = iSrc.height;
+			var sW = rect.width;
+			var sH = rect.height;
+			var sX = rect.x;
+			var sY = rect.y;
+			var sC = {x:sX + (sW/2), y:sY + (sH/2)};
+		
+			var scale = tH/sH;
 
-		var sDW = sW*scale;
-		var sDH = sW*scale;
-		var sDC = {x:rect.x*scale + ((rect.width*scale)/2), y:rect.y*scale + ((rect.height*scale)/2)}
+			var sDW = sW*scale;
+			var sDH = sW*scale;
+			var sDC = {x:rect.x*scale + ((rect.width*scale)/2), y:rect.y*scale + ((rect.height*scale)/2)}
 		
-	//	var siDX = tC.x - (sDC.x/2+sX*scale);
-	//	var siDY = tC.y - (sDC.y/2+sY*scale);
+		//	var siDX = tC.x - (sDC.x/2+sX*scale);
+		//	var siDY = tC.y - (sDC.y/2+sY*scale);
 		
-		var siDX = tX - rect.x*scale;
-		var siDY = tY - rect.y*scale;
+			var siDX = tX - rect.x*scale;
+			var siDY = tY - rect.y*scale;
 			
-	//	var sDX = tC.x - (sDC.x/2);
-	//	var sDY = tC.y - (sDC.y/2);
+		//	var sDX = tC.x - (sDC.x/2);
+		//	var sDY = tC.y - (sDC.y/2);
 		
-		var sDX = tX ;
-		var sDY = tY;
+			var sDX = tX ;
+			var sDY = tY;
 		
-		ctx.drawImage(iTg,0,0);
-		if(debug){
-			ctx.strokeStyle="red";
-			ctx.rect(tX,tY, tW, tH);
-			ctx.stroke();
-		}
-		ctx.globalCompositeOperation = "overlay";
-		ctx.drawImage(iSrc,siDX,siDY,siW*scale,siH*scale);
 		
-		if(debug){
-	
+			function maskPaint(){
+				var pad = 20;
+				ctx.globalCompositeOperation = "source-over";
+				ctx.save();
+
+				ctx.beginPath();
+				ctx.moveTo(tX-pad, tY-pad);
+				ctx.lineTo(tX+tW+pad,tY-pad);
+				/// ... more here - see demo
+				ctx.lineTo(tX+tW+pad, tY+tH+pad);
+				ctx.lineTo(tX-pad, tY+tH+pad);
+				ctx.lineTo(tX-pad,tY+pad);
+				ctx.closePath();
+
+				/// define this Path as clipping mask
+				ctx.clip();
+				
+				/// draw the image
+				ctx.drawImage(iPaint, 0, 0);
+
+				/// reset clip to default
+				ctx.restore();
+			}
 			
-			ctx.strokeStyle="purple";
-			ctx.rect(sDX,sDY,sDW,sDH);
-			ctx.stroke();
+			function drawFace(){
+				if(debug){
+					ctx.strokeStyle="red";
+					ctx.rect(tX,tY, tW, tH);
+					ctx.stroke();
+				}
+				ctx.globalCompositeOperation = blendMode;
+				ctx.drawImage(iSrc,siDX,siDY,siW*scale,siH*scale);
+				
+				if(debug){
+
+
+					ctx.strokeStyle="purple";
+					ctx.rect(sDX,sDY,sDW,sDH);
+					ctx.stroke();
+				}
+				
+			}
+			maskPaint();
+			drawFace();
+			
+		
+		
+		//	clone_dfd.resolve(ctx);
 		}
-		
-		
-		dfd.resolve(cnv.toDataURL());
+	//	return clone_dfd.promise();
 	}
-	
-		
 	return dfd.promise();
 }
 
